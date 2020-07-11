52db5c0bdfbc28e2b6601fe0cf9d557cddca7107
Sergei Shinkarenko <serewja@gmail.com>
2020-07-11 13:46:58 +0500

    add tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,12 @@
-import f from "../dist/index.js"
+import finalGrade from "../dist/index.js"
 
 test('testname', () => {
-    
+  expect(finalGrade(100, 12)).toBe(100);
+  expect(finalGrade(99, 0)).toBe(100);
+  expect(finalGrade(10, 15)).toBe(100);
+  expect(finalGrade(90, 10)).toBe(90);
+  expect(finalGrade(85, 5)).toBe(90);
+  expect(finalGrade(55, 3)).toBe(75);
+  expect(finalGrade(55, 0)).toBe(0);
+  expect(finalGrade(20, 2)).toBe(0);
 });
